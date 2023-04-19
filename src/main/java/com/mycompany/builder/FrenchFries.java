13ce70f2409d1a1c19a13afd6966613c657fa5e6
alu10701951 <alu10701951@ti3-03>
2023-04-19 20:16:37 +0200

    1 commit

---
+++ src/main/java/com/mycompany/builder/FrenchFries.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.builder;
+
+/**
+ *
+ * @author alu10701951
+ */
+public class FrenchFries extends Fries{
+
+    @Override
+    public float price() {
+        return 2.0f;
+    }
+
+    @Override
+    public String name() {
+        return "French Fries";
+    }
+    
+
+    
+    
+}
